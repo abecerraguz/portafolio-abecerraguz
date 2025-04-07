a13cb6ced5cb1439cbccb2a0a361bb3c27a53abb
abecerraguz <alejandrobecerrabecerra73@gmail.com>
2025-04-07 15:45:51 -0400

    Se actualiza el Meta title, description, generator

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Portafolio Alejandro Becerra',
+  description: 'Portafolio desarrollado con NextJS, Tailwind, TypeScript',
+  generator: 'NextJS 15.2.4',
 }
 
 export default function RootLayout({
