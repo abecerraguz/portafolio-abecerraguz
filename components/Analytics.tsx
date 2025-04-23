3861122f95e6765e068292d735653675730b08af
abecerraguz <alejandrobecerrabecerra73@gmail.com>
2025-04-23 07:31:56 -0400

    Se quita el searchParams en el componente Analytics.tsx para que no se rompa el build

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import { usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
 
 declare global {
   interface Window {
@@ -11,26 +11,14 @@ declare global {
 
 export default function Analytics() {
   const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  const [isClient, setIsClient] = useState(false)
 
   useEffect(() => {
-    // Marcar que ya estamos en el cliente
-    setIsClient(true)
-  }, [])
-
-  useEffect(() => {
-    if (!isClient) return
-
-    const url = `${pathname}${searchParams.toString() ? '?' + searchParams.toString() : ''}`
-
     if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
       window.gtag('config', 'G-M8Q7W58PFP', {
-        page_path: url,
+        page_path: pathname,
       })
     }
-  }, [isClient, pathname, searchParams])
+  }, [pathname])
 
   return null
 }
